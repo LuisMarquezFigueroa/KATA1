ff58fdac9816269651631f14b52b399a004c395d
luis <luis@DESKTOP-DSCGUEK.mshome.net>
2019-10-02 21:37:15 +0100

    kata1

---
+++ src/kata001/KATA001.java
@@ -0,0 +1,18 @@
+package KATA001;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Usuario
+ */
+public class KATA001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Person pau = new Person("Pau Gasol", new Date(84,1,10));
+    }
+   
+}
